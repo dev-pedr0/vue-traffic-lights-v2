8ac1d7b8828c3e41186c405968a42c6575230106
Pedro Costa <pedro4costa@outlook.com>
2024-02-06 09:36:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+let bola = {
+    props: ['cor'],
+    template:`
+    <div class="bola" v-bind:class="cor">
+
+    </div>
+    `
+};
+
+Vue.component('sinal', {
+    props:['status'],
+    components: {
+        bola
+    },
+    template:`
+        <div class="sinal">
+            <template v-if="status == 'red'">
+                <bola cor="red"></bola>
+                <bola></bola>
+                <bola></bola>
+            </template>
+            <template v-if="status == 'yellow'">
+                <bola></bola>
+                <bola cor="yellow"></bola>
+                <bola></bola>
+            </template>
+            <template v-if="status == 'green'">
+                <bola></bola>
+                <bola></bola>
+                <bola cor="green"></bola>
+            </template>
+        </div>
+    `
+});
+
+let app = new Vue ({
+    el: '#app',
+    data: {
+        sinalStatus: 'red'
+    },
+    methods: {
+        liberar: function () {
+            this.sinalStatus = 'green';
+        },
+        fechar: function () {
+            this.sinalStatus = 'yellow';
+            setTimeout(this.fecharSinal, 1000);
+        },
+        fecharSinal: function () {
+            this.sinalStatus = 'red';
+        }
+    }
+});
